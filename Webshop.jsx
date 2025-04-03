5747d81dac6973f4475b52969f83cb3b77b82cc3
valhalla-diamond <mtamasmadaras@gmail.com>
2025-04-03 09:08:19 +0200

    Add files via upload

---
+++ Webshop.jsx
@@ -0,0 +1,11 @@
+
+import { useState } from 'react';
+
+export default function Webshop() {
+  return (
+    <div className="p-4 text-center">
+      <h1 className="text-3xl font-bold mb-4">Valhalla Diamond Webshop</h1>
+      <p>Termékfeltöltés folyamatban...</p>
+    </div>
+  );
+}
